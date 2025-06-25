838272563c13a52fef9842961826d2b6868f773f
Eliana Puerta <epuerta@prevalentware.com>
2025-06-24 21:23:31 -0500

    fix:login

--- src/test/scala/Demo/Data.scala
+++ src/test/scala/Demo/Data.scala
@@ -2,6 +2,6 @@ package Demo
 
 object Data{
     val url= "https://thinking-tester-contact-list.herokuapp.com"
-    val username = "eliana@gmail.com"
+    val email = "eliana@gmail.com"
     val password = "Test12345"
 }
